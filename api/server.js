a716727c05a5afae4c4de9dc37835e77894de608
Scott Smith <scthsm@outlook.com>
2020-04-23 17:11:07 -0600

    a

--- api/server.js
+++ api/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 
 const Cheese = require("../cheese/cheeseModel.js");
-
+const helmet = require("helmet");
+const cors = require("cors");
 const server = express();
 
+server.use(helmet());
 server.use(express.json());
+server.use(cors())
 
 server.get("/", (req, res) => {
   res.status(200).json({ api: "up" });
@@ -21,8 +24,37 @@ server.get("/cheese", (req, res) => {
 });
 
 server.post("/cheese", (req, res) => {
-      res.status(201).json({message:"Cheese created successfully"}); 
-});
+    const cheeseData = req.body;
+    Cheese.insert(cheeseData)
+    .then(cheese => {
+        res.status(201).json({message:"Cheese added successfully",cheese});
+    })
+    .catch (err => {
+      console.log('POST error', err);
+      res.status(500).json({ message: "Failed to store cheese data" });
+    });
+  });
 
+  server.delete('/:id', (req, res) => {
+    Cheese.remove(req.params.id)
+    .then(cheese => {
+      if (cheese.length == 0) {
+        res.status(404).json({
+          message: "No cheese Found"
+        });
+      } else {
+        res.status(200).json({
+          message: "cheese deleted"
+        });
+      }
+    })
+    .catch(error => {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: 'Error removing the cheese',
+      });
+    });
+  });
 
 module.exports = server;
--- cheese/cheeseModel.js
+++ cheese/cheeseModel.js
@@ -5,27 +5,36 @@ module.exports = {
   update,
   remove,
   getAll,
-  findById,
+  getById,
 };
 
 async function insert(cheese) {
-  return db('cheese')
-  .insert(cheese, 'id')
-  .then(([id]) => get(id));
-}
+    return db('cheese')
+      .insert(cheese)
+      .then(ids => {
+        return getById(ids[0]);
+      });
+  }
 
 async function update(id, changes) {
-  return null;
-}
+    return db('cheese')
+      .where({ id })
+      .update(changes);
+  }
 
 function remove(id) {
-  return null;
-}
+    return db('cheese')
+      .where('id', id)
+      .del();
+  }
+  
 
 function getAll() {
   return db('cheese');
 }
 
-function findById(id) {
-  return null;
-}
+function getById(id) {
+    return db('cheese')
+      .where({ id })
+      .first();
+  }
